5cb0a0b7836236154cfbcae3b2b8be80fb2818b0
Andrei Dziaineka <a_dziaineka@wargaming.net>
2020-08-15 23:19:07 +0300

    group id instead of str username

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from os import getenv
 
 BOT_TOKEN = getenv('BOT_TOKEN')
-DOCTORS_GROUP = getenv('DOCTORS_GROUP')
+DOCTORS_GROUP = int(getenv('DOCTORS_GROUP', 0))
 RESERVE_CHANNEL = int(getenv('RESERVE_CHANNEL', 0))
